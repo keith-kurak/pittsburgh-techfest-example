b125d8d1fbee0e82492cf0f28885ee45691c6b90
Keith Kurak <keith.kurak@gmail.com>
2018-06-01 01:00:53 -0400

    reorganized, added nice dev dependency stuff

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import Storyboard from './storyboard';
+import Storyboard from './app/storyboard';
 
 export default class App extends Component {
   render() {
